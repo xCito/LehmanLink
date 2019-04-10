49f062486d0573a091142814cf09bcfc5b0dfd66
Juan Lantigua <juanlantiguajr@gmail.com>
2019-04-10 19:35:39 -0400

    Added __str__() to some of the models

--- mysite/lehmanlinkapp/admin.py
+++ mysite/lehmanlinkapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Lehmanuser
+from .models import Department
+from .models import Course
+from .models import Coursesection
+from .models import Student
+from .models import Instructor
+from .models import Alumni
+
 
 # Register your models here.
+admin.site.register(Lehmanuser)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Coursesection)
+admin.site.register(Student)
+admin.site.register(Instructor)
+admin.site.register(Alumni)
--- mysite/lehmanlinkapp/models.py
+++ mysite/lehmanlinkapp/models.py
@@ -1,12 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Lehmanuser(models.Model):
+    emplid = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    photo = models.CharField(max_length=50, blank=True, null=True)
+
+    # like the toString() from java
+    def __str__(self):
+        output = "\n" +str(self.emplid) +"\t"+ self.username +"\t"+ self.email +"\t"+ self.phone +"\t"+ self.photo
+        return output
+
+
+    class Meta:
+        managed = False
+        db_table = 'lehmanuser'
+
+# ---------------------------------------------------------
+
+class Department(models.Model):
+    dept_name = models.CharField(unique=True, max_length=30)
+
+    def __str__(self):
+        output = "\n" + self.dept_name
+        return output
+
+    class Meta:
+        managed = False
+        db_table = 'department'
+
+# ---------------------------------------------------------
+
 class Student(models.Model):
     user = models.ForeignKey(Lehmanuser, models.DO_NOTHING, primary_key=True)
-    major_dept = models.ForeignKey(Department, models.DO_NOTHING)
-    minor_dept = models.ForeignKey(Department, models.DO_NOTHING, blank=True, null=True)
+    major_dept = models.ForeignKey(Department, models.DO_NOTHING, related_name="major")
+    minor_dept = models.ForeignKey(Department, models.DO_NOTHING, blank=True, null=True, related_name="minor")
     major_vis = models.IntegerField()
     minor_vis = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        output = "\n" + str(self.user.emplid) +"\t"+ self.major_dept+"\t"+ self.minor_dept +"\t"+self.major_vis+"\t"+self.minor_vis
+        return output
 
     class Meta:
         managed = False
@@ -22,6 +59,10 @@ class Experience(models.Model):
     from_field = models.DateField(db_column='from')  # Field renamed because it was a Python reserved word.
     to = models.DateField()
 
+    def __str__(self):
+        output = "\n" + str(self.emplid) +"\t"+ self.title+"\t"+ self.description +"\t"+ str(self.from_field)+"\t"+self.to
+        return output
+
     class Meta:
         managed = False
         db_table = 'experience'
@@ -33,6 +74,10 @@ class Instructor(models.Model):
     user = models.ForeignKey('Lehmanuser', models.DO_NOTHING, primary_key=True)
     dept = models.ForeignKey(Department, models.DO_NOTHING)
 
+    def __str__(self):
+        output = "\n" + str(self.emplid) +"\t"+dept.dept_name
+        return output
+
     class Meta:
         managed = False
         db_table = 'instructor'
@@ -40,16 +85,14 @@ class Instructor(models.Model):
 
 # ---------------------------------------------------------
 
-class Lehmanuser(models.Model):
-    emplid = models.IntegerField(primary_key=True)
-    username = models.CharField(max_length=45)
-    email = models.CharField(max_length=45)
-    phone = models.CharField(max_length=15, blank=True, null=True)
-    photo = models.CharField(max_length=50, blank=True, null=True)
+class Course(models.Model):
+    course_name = models.CharField(max_length=10)
+    dept = models.ForeignKey('Department', models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'lehmanuser'
+        db_table = 'course'
+        unique_together = (('id', 'dept'),)
 
 # ---------------------------------------------------------
 
@@ -65,26 +108,6 @@ class Coursesection(models.Model):
         managed = False
         db_table = 'coursesection'
         unique_together = (('user', 'instruct', 'course'),)
-
-# ---------------------------------------------------------
-
-class Department(models.Model):
-    dept_name = models.CharField(unique=True, max_length=30)
-
-    class Meta:
-        managed = False
-        db_table = 'department'
-
-# ---------------------------------------------------------
-
-class Course(models.Model):
-    course_name = models.CharField(max_length=10)
-    dept = models.ForeignKey('Department', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'course'
-        unique_together = (('id', 'dept'),)
 
 # ---------------------------------------------------------
 
